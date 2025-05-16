0e6aa1a47fef43d2556b47ee5fba147f2bdd02c5
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-05-16 12:02:43 +0500

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/temp'
+import ChatContainer from '../components/ChatContainer'
 import NoChatSelected from '../components/NoChatSelected'
 import { useChatStore } from '../store/useChatStore'
 
